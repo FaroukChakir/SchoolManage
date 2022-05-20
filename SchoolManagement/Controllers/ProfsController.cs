5f157095068600a6efa348d5621fd8bb5fa341c4
FaroukChakir <faroukchakir@gmail.com>
2022-05-20 14:28:44 +0100

    last

--- SchoolManagement/Controllers/ProfsController.cs
+++ SchoolManagement/Controllers/ProfsController.cs
@@ -7,6 +7,12 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using SchoolManagement;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.xml;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.xml.simpleparser;
+using System.IO;
 
 namespace SchoolManagement.Controllers
 {
@@ -17,8 +23,9 @@ namespace SchoolManagement.Controllers
         // GET: Profs
         public ActionResult Index()
         {
-            var prof = db.Prof.Include(p => p.matiere);
-            return View(prof.ToList());
+            ViewBag.matiere = db.matiere.ToList();
+            //var prof = db.Prof.Include(p => p.matiere);
+            return View(db.Prof.ToList());
         }
 
         // GET: Profs/Details/5
@@ -39,7 +46,7 @@ namespace SchoolManagement.Controllers
         // GET: Profs/Create
         public ActionResult Create()
         {
-            ViewBag.id_matiere = new SelectList(db.matiere, "id", "nom");
+            ViewBag.matiere = db.matiere.ToList();
             return View();
         }
 
@@ -47,78 +54,226 @@ namespace SchoolManagement.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "CNE,id_matiere,name,email,adresse,login,password,Tel")] Prof prof)
+        public ActionResult Create(Prof prof)
         {
+
+
+            string message = "";
             if (ModelState.IsValid)
             {
-                db.Prof.Add(prof);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+                try
+                {
 
-            ViewBag.id_matiere = new SelectList(db.matiere, "id", "nom", prof.id_matiere);
-            return View(prof);
+
+                    db.Prof.Add(prof);
+
+                    db.SaveChanges();
+                    message = "Ajouter avec succés";
+
+
+                    return this.Json(new { CNE = prof.CNE });
+                }
+                catch (Exception ex) { message = "Error!"; }
+            }
+            else
+            {
+                message = "Error.";
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return new JsonResult { Data = message };
+            }
+            else
+            {
+                ViewBag.Message = message;
+                return View(prof);
+                //return Json(new { id = absence.Id });
+            }
         }
 
         // GET: Profs/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Prof prof = db.Prof.Find(id);
-            if (prof == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.id_matiere = new SelectList(db.matiere, "id", "nom", prof.id_matiere);
-            return View(prof);
-        }
+       
 
         // POST: Profs/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CNE,id_matiere,name,email,adresse,login,password,Tel")] Prof prof)
+        public ActionResult Edit(Prof prof)
         {
+            string message = "";
             if (ModelState.IsValid)
             {
-                db.Entry(prof).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                try
+                {
+
+
+                    Prof ToModify_Prof = db.Prof.Find(prof.CNE);
+
+                    ToModify_Prof.name = prof.name;
+                    ToModify_Prof.CNE = prof.CNE;
+                    ToModify_Prof.id_matiere = prof.id_matiere;
+                    ToModify_Prof.email = prof.email;
+                    ToModify_Prof.adresse = prof.adresse;
+                    ToModify_Prof.login = prof.login;
+                    ToModify_Prof.password = prof.password;
+                    ToModify_Prof.Tel = prof.Tel;
+
+
+                    db.SaveChanges();
+                    message = "Ajouter avec succés";
+
+
+                    return Json(prof);
+                }
+                catch (Exception ex) { message = "Error!"; }
             }
-            ViewBag.id_matiere = new SelectList(db.matiere, "id", "nom", prof.id_matiere);
-            return View(prof);
+            else
+            {
+                message = "Error.";
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return new JsonResult { Data = message };
+            }
+            else
+            {
+                ViewBag.Message = message;
+                return View(prof);
+                //return Json(new { id = absence.Id });
+            }
         }
 
         // GET: Profs/Delete/5
+        [HttpPost]
+        [Route("Profs/Delete/{id}")]
         public ActionResult Delete(string id)
         {
-            if (id == null)
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            //Etudiant etudiant = db.Etudiant.Find(id);
+            //if (etudiant == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            //return View(etudiant);
+
+            Prof prof = db.Prof.SingleOrDefault(c => c.CNE == id);
+            if (prof != null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+                db.Prof.Remove(prof);
+                if (db.SaveChanges() > 0)
+                {
+                    return Json(new { error = false, Message = "Cet absence est supprimé avec succès" });
+                }
+                else
+                {
+                    return Json(new { error = true, Message = "Il y a une erreur dans la sauvegarde des données" });
+                }
             }
-            Prof prof = db.Prof.Find(id);
-            if (prof == null)
+            else
             {
-                return HttpNotFound();
+                return Json(new { error = true, Message = "Ce formateur n'existe pas" });
             }
-            return View(prof);
         }
 
-        // POST: Profs/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public FileResult Export()
         {
-            Prof prof = db.Prof.Find(id);
-            db.Prof.Remove(prof);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            using (MemoryStream stream = new MemoryStream())
+            {
+
+
+
+                Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+
+                BaseFont bf = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+                PdfPTable table = new PdfPTable(8);
+
+
+                Font font = new Font(bf, 20, Font.NORMAL, BaseColor.RED);
+
+
+                PdfPCell cell0 = new PdfPCell(new Phrase("List des Etudiants", font));
+                cell0.Colspan = 8;
+                cell0.HorizontalAlignment = PdfPCell.ALIGN_CENTER;
+                cell0.Border = PdfPCell.NO_BORDER;
+                cell0.Padding = 5;
+                table.AddCell(cell0);
+
+                //table.AddCell(prof.CNE);
+                //table.AddCell(prof.name);
+                //table.AddCell(prof.matiere.nom);
+                //table.AddCell(prof.email);
+                //table.AddCell(prof.adresse);
+                //table.AddCell(prof.login);
+                //table.AddCell(prof.password);
+                //table.AddCell(prof.Tel);
+
+                PdfPCell cell1 = new PdfPCell(new Phrase("CNE"));
+                cell1.BackgroundColor = BaseColor.YELLOW;
+                table.AddCell(cell1);
+
+                PdfPCell cell2 = new PdfPCell(new Phrase("name"));
+                cell2.BackgroundColor = BaseColor.YELLOW;
+                table.AddCell(cell2);
+
+                PdfPCell cell3 = new PdfPCell(new Phrase("matiere"));
+                cell3.BackgroundColor = BaseColor.YELLOW;
+                table.AddCell(cell3);
+
+                PdfPCell cell4 = new PdfPCell(new Phrase("email"));
+                cell4.BackgroundColor = BaseColor.YELLOW;
+                table.AddCell(cell4);
+
+                PdfPCell cell5 = new PdfPCell(new Phrase("adresse"));
+                cell5.BackgroundColor = BaseColor.YELLOW;
+                table.AddCell(cell5);
+
+                PdfPCell cell6 = new PdfPCell(new Phrase("login"));
+                cell6.BackgroundColor = BaseColor.YELLOW;
+                table.AddCell(cell6);
+
+                PdfPCell cell7 = new PdfPCell(new Phrase("password"));
+                cell7.BackgroundColor = BaseColor.YELLOW;
+                table.AddCell(cell7);
+
+                PdfPCell cell8 = new PdfPCell(new Phrase("Tel"));
+                cell8.BackgroundColor = BaseColor.YELLOW;
+                table.AddCell(cell8);
+
+                foreach (Prof prof in db.Prof.ToList())
+                {
+                    table.AddCell(prof.CNE);
+                    table.AddCell(prof.name);
+                    table.AddCell(prof.matiere.nom);
+                    table.AddCell(prof.email);
+                    table.AddCell(prof.adresse);
+                    table.AddCell(prof.login);
+                    table.AddCell(prof.password);
+                    table.AddCell(prof.Tel);
+                }
+                string todaydate = DateTime.Now.Day.ToString() + '/' + DateTime.Now.Month.ToString() + '/' + DateTime.Now.Year.ToString();
+
+                PdfPCell cell9 = new PdfPCell(new Phrase(todaydate));
+                cell9.Colspan = 8;
+                cell9.Border = PdfPCell.NO_BORDER;
+                table.AddCell(cell9);
+                pdfDoc.Add(table);
+
+                pdfDoc.Close();
+                return File(stream.ToArray(), "application/pdf", "List Etudiants.pdf");
+            }
         }
+
+
+
 
         protected override void Dispose(bool disposing)
         {
